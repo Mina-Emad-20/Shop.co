91c37c3f58aacb25e80ed4a44d3065bee9b00bea
Mina-Emad-20 <167125155+Mina-Emad-20@users.noreply.github.com>
2024-08-19 21:17:39 +0300

    edit on APIs

--- src/api/getProducts.js
+++ src/api/getProducts.js
@@ -1,5 +1,5 @@
-export const getProducts = (setProducts) => fetch('http://localhost:3000/products').then(res => res.json()).then(res => setProducts(res))
+export const getProducts = (setProducts) => fetch('https://fakestoreapi.com/products').then(res => res.json()).then(res => setProducts(res))
 
-export const getSingleProduct = (setProduct, id) => fetch('http://localhost:3000/products/' + id).then(res => res.json()).then(res => setProduct(res))
+export const getSingleProduct = (setProduct, id) => fetch('https://fakestoreapi.com/products/' + id).then(res => res.json()).then(res => setProduct(res))
 
 export const ratingReviews = (setReviews) => fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(res => setReviews(res))
